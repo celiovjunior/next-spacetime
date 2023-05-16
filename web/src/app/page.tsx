92e934a3716bd9eb468bb29ddeaf3a525be15cb5
Célio V. Júnior <celio_cjr@hotmail.com>
2023-05-16 21:17:14 +1200

    nextreact: config prettier

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-2xl p-4 bg-zinc-800 text-white uppercase w-20 rounded text-center">
-      oie
+    <div className="p-4 bg-zinc-800 text-white uppercase w-20 rounded text-center">
+      hello world!
     </div>
   )
 }
