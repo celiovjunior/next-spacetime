14372099974a43094b010fac9fbaf0d60b2322c4
Célio V. Júnior <celio_cjr@hotmail.com>
2023-05-17 17:10:10 +1200

    front: using tailwind to make blur effect at background

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-200`}>{children}</body>
     </html>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <main>
-      
+    <main className="grid grid-cols-2 min-h-screen">
+      {/** Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
+
+        <div className="rounded-full absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 blur-full">
+
+        </div>
+      </div>
+
+      {/* Right*/}
+      <div className="flex flex-col p-16">
+        <div className="flex h-full items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">Você ainda não registrou nenhuma lembrança, comece a {' '} <a href="" className="underline hover:text-gray-50">criar agora!</a></p>
+        </div>
+      </div>
     </main>
   )
 }
