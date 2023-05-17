fa2c84b380c60de9fc788018bfe385fc37a24414
Célio V. Júnior <celio_cjr@hotmail.com>
2023-05-17 16:33:24 +1200

    set up layout, font, new colors and more

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-zinc-800 text-white uppercase w-20 rounded text-center">
-      hello world!
-    </div>
+    <main>
+      
+    </main>
   )
 }
