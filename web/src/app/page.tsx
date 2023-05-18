e59bc6dbab7ceccef5c9fd6dba73b92018271bb9
Célio V. Júnior <celio_cjr@hotmail.com>
2023-05-18 17:05:33 +1200

    auth: set up access token for client and app auth github

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
 
         {/* Sign In */}
-        <a href="" className="hover:text-gray-50 flex items-center gap-3 text-left">
+        <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`} className="hover:text-gray-50 flex items-center gap-3 text-left">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
