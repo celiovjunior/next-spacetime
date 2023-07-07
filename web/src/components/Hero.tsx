4498a8bf426ff64f5f2d02f35a9c3ec95b0eab74
Celio Junior <celio_cjr@hotmail.com>
2023-07-07 19:56:30 +1200

    fix: import logo image as component

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import logo from '../assets/logo.svg';
+
 import Link from "next/link";
+import Logo from "./logo";
 
 
 export function Hero() {
   return (
     <div className="space-y-5">
-      <Image src={logo} alt="NLW Event's logo" />
+      <Logo />
 
       <div className="max-w-[420px] space-y-4">
         <h1 className="mt-5 text-4xl font-bold leading-tight text-gray-50">Your time capsule</h1>
