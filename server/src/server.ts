19b982ef87c1c1c701b242f925253cb35fb28bb1
Célio Júnior <celio_cjr@hotmail.com>
2023-09-18 23:56:54 +1200

    dev: building the server with new libs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,18 @@
 import fastify from 'fastify'
-import cors from '@fastify/cors'
-import 'dotenv/config'
-import { uploadRoutes } from './routes/upload'
-import { memoriesRoutes } from './routes/memories'
-import { authRoute } from './routes/auth'
-import { fastifyJwt as jwt } from '@fastify/jwt'
-import multipart from '@fastify/multipart'
-import { resolve } from 'path'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-// in production, put the host url
-app.register(cors, {
-  origin: true,
-})
-
-app.register(require('@fastify/static'), {
-  root: resolve(__dirname, '../uploads'),
-  prefix: '/uploads',
-})
-
-app.register(multipart) // lib to upload files
-app.register(memoriesRoutes)
-app.register(uploadRoutes)
-app.register(authRoute)
-app.register(jwt, {
-  secret: 'spacetime',
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
   })
   .then(() => {
-    console.log('🐎 Server is running at http://localhost:3333')
+    console.log('🏇 HTTP server is running')
   })
