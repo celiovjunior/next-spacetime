429c1f56e92e128f31daf1c0648952da7f3bc379
Célio Júnior <celio_cjr@hotmail.com>
2023-09-19 00:43:48 +1200

    feat: adding read one memory functionality

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { z } from 'zod'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
@@ -9,12 +10,33 @@ export async function memoriesRoutes(app: FastifyInstance) {
       },
     })
 
-    return memories
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
   })
-  app.get('/memories/:id', async () => {
-    const users = await prisma.user.findMany()
-    return users
+
+  app.get('/memories/:id', async (request, reply) => {
+    // const { id } = request.params
+
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
   })
+
   app.post('/memories', async () => {
     const users = await prisma.user.findMany()
     return users
